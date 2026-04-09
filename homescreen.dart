792a680881d7b40bea4516821adc042a2c508674
atharvvermahere <atharv6108@gmail.com>
2026-04-09 18:42:13 +0530

    home screen

---
+++ homescreen.dart
@@ -0,0 +1,55 @@
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> _history = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHistory();
+  }
+
+  void _loadHistory() async {
+    final history = await QRService.getHistory();
+    setState(() => _history = history);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("QR Utility")),
+      body: Column(
+        children: [
+          // Action Buttons [cite: 8, 9]
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ScannerScreen())),
+                child: Text("Scan QR"),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => GeneratorScreen())),
+                child: Text("Generate QR"),
+              ),
+            ],
+          ),
+          Divider(),
+          Text("Recent Scans", style: TextStyle(fontWeight: FontWeight.bold)), // [cite: 10]
+          Expanded(
+            child: ListView.builder(
+              itemCount: _history.length,
+              itemBuilder: (context, index) => ListTile(
+                leading: Icon(Icons.history),
+                title: Text(_history[index]),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
